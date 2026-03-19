60f1e45c0351c586f5b90d289a1ebafcb83fcd37
akibeulah <akibeulah@gmail.com>
2026-03-19 20:43:18 +0100

    Updated BaseURL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const baseURL = import.meta.env.DEV
+  ? 'http://localhost:5000/api'
+  : 'https://hallmanagementsystemserver.onrender.com/api';
+
+const api = axios.create({ baseURL });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
